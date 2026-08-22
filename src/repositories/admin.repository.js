c3502a4ed9ee7917bb92372dd6e9c73eda5a19ed
akira4n <yobiijo94@gmail.com>
2026-08-22 16:02:57 +0700

    refactor: simplify by use shorthand property

--- src/repositories/admin.repository.js
+++ src/repositories/admin.repository.js
@@ -2,7 +2,7 @@ const prisma = require("../config/prisma");
 
 const findAdminByEmail = async (email) => {
   return await prisma.admin.findUnique({
-    where: { email: email },
+    where: { email },
   });
 };
 
