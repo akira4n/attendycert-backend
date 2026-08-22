29468626a2a921bd07c3654bf7978fe898da2aa3
akira4n <yobiijo94@gmail.com>
2026-08-22 16:04:23 +0700

    feat: add validation middleware to login route

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin.controller');
+const validate = require('../middlewares/validate.middleware');
+const { loginSchema } = require('../validations/auth.validation');
 
-router.post('/login', adminController.login);
+router.post('/login', validate(loginSchema), adminController.login);
 
 module.exports = router;
