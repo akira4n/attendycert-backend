3b4546c5dcfe8335098dc987aca68dde6c8a9395
akira4n <yobiijo94@gmail.com>
2026-09-05 17:53:53 +0700

    feat: reorganize event routes for clarity and add admin event route handlers

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -5,12 +5,17 @@ const validate = require('../middlewares/validate.middleware');
 const { eventSchema } = require('../validations/event.validation');
 const protectAdmin = require('../middlewares/auth.middleware');
 
-router.get('/:slug', eventController.getEventBySlug);
+// admin routes
+router.get('/admin', protectAdmin, eventController.getAllEventsAdmin);
+router.get('/admin/:id', protectAdmin, eventController.getEventById);
 router.post(
   '/',
   protectAdmin,
   validate(eventSchema),
   eventController.createEvent,
 );
+
+// public routes
+router.get('/:slug', eventController.getEventBySlug);
 
 module.exports = router;
