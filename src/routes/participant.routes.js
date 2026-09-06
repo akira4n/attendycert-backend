b680f101ad3cdc695c3f73d939ba1f30f5f19897
akira4n <yobiijo94@gmail.com>
2026-09-06 17:12:35 +0700

    feat: add admin routes for participant check-in and retrieval

--- src/routes/participant.routes.js
+++ src/routes/participant.routes.js
@@ -6,13 +6,30 @@ const validate = require('../middlewares/validate.middleware');
 const { uploadAny } = require('../middlewares/upload.middleware');
 const {
   registerParticipantSchema,
+  checkInSchema,
 } = require('../validations/participant.validation');
+const protectAdmin = require('../middlewares/auth.middleware');
 
+// public routes
 router.post(
   '/:slug/register',
   uploadAny.any(),
   validate(registerParticipantSchema),
   participantController.registerParticipant,
+);
+
+// admin routes
+router.post(
+  '/:eventId/check-in',
+  protectAdmin,
+  validate(checkInSchema),
+  participantController.checkIn,
+);
+
+router.get(
+  '/:eventId/participants',
+  protectAdmin,
+  participantController.getParticipants,
 );
 
 module.exports = router;
